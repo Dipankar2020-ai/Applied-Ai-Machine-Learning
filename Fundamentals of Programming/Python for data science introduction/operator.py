0e80d8e310d835004645a13fa592485cc8448e42
Dipankar2020-ai <63770793+Dipankar2020-ai@users.noreply.github.com>
2021-11-28 09:22:29 +0530

    Add files via upload

---
+++ Fundamentals of Programming/Python for data science introduction/operator.py
@@ -0,0 +1,39 @@
+Python 3.9.6 (tags/v3.9.6:db3ff76, Jun 28 2021, 15:26:21) [MSC v.1929 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> a=10;b=20
+>>> print(a)
+10
+>>> a/b
+0.5
+>>> print(45%7)
+3
+>>> print(78//8)
+9
+>>> print(-78//8)
+-10
+>>> a+=b
+>>> print(a)
+30
+>>> c=20
+>>> print(b is c)
+True
+>>> d=[1,2,3]
+>>> e=[1,2,3]
+>>> print(d is e)
+False
+>>> print(2**3)
+8
+>>> s1="Raju",s2="Raju"
+SyntaxError: cannot assign to literal
+>>> s1="Raju";s2="Raju"
+>>> print(s1 is s2)
+True
+>>> g=[1,2,3,4]
+>>> print(1 in g)
+True
+>>> print(2 in g)
+True
+>>> d={1:'a',2:'b'}
+>>> print(2 in d)
+True
+>>> 
