cf729d9515ec08b6a98360d6b3948e86448d0b16
Dipankar2020-ai <63770793+Dipankar2020-ai@users.noreply.github.com>
2021-12-16 20:17:51 +0530

    Add files via upload

---
+++ Fundamentals of Programming/Python for Data Science_ Functions/modules.py
@@ -0,0 +1,12 @@
+import math
+print(math.pi)
+import datetime
+print(datetime.datetime.now())
+import math as m
+print(m.pi)
+from datetime import datetime
+print(datetime.now())
+from math import*
+print("value of pi is "+str(pi))
+dir(math)
+print(math.atan.__doc__)
